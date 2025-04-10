9aa9fce0c0bae790f6f2a75d9551ee0a826d4008
chriswiesanjaya <chriswiesanjaya@gmail.com>
2025-04-10 17:00:41 +1000

    fix global singleton prisma

--- app/api/eventBrite/route.ts
+++ app/api/eventBrite/route.ts
@@ -1,13 +1,5 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client-community";
-
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+import { prisma } from "@/lib/prisma";
 
 interface RawEventResult {
     event_id: string;
